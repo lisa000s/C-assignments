28e95c5ad4f9e1a81736280349e4ba0abf527207
vasilinka86 <vasilinka86@gmail.com>
2016-06-01 20:52:08 -0400

    Used cout output op

--- ClionProjects/C Programming/main.cpp
+++ ClionProjects/C Programming/main.cpp
@@ -14,15 +14,18 @@ int main() {
 
     //calculate area of the circle
     circle_area = pi * circle_radius * circle_radius;
-    printf("The area of the circle is: %f \n", circle_area);
+    cout << "The area of the circle is: " << circle_area << endl;
+    //printf("The area of the circle is: %f \n", circle_area);
 
     //calculate area of the square containing circle
     square_area = (2 * circle_radius) * (2 * circle_radius);
-    printf("The area of the square is: %d \n", square_area);
+    cout << "The area of the square is: " << square_area << endl;
+    //printf("The area of the square is: %d \n", square_area);
 
     //calculate the difference
     difference_area = square_area - circle_area;
-    printf("The difference is: %f \n", difference_area);
+    cout << "The difference is: " << difference_area << endl;
+    //printf("The difference is: %f \n", difference_area);
 
     return 0;
 }
